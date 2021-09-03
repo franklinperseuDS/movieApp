eae81ce1a561f38c9733f81af36ed7215bcfebe9
Franklin Perseu de Lima e Lima <69854802+franklinperseuDS@users.noreply.github.com>
2021-09-03 08:30:13 -0400

    alterando cod principal

--- app.py
+++ app.py
@@ -31,22 +31,22 @@ if btn_predict:
         }
     }
 
-body = str.encode(json.dumps(data))
+    body = str.encode(json.dumps(data))
 
-url = 'https://ussouthcentral.services.azureml.net/workspaces/88cee1267b3f4781be5764f390ed1fa2/services/13bef76ee21148a6ba961ad4bdd6f0fa/execute?api-version=2.0&format=swagger'
-api_key = 'v+QeyVdnxNfouQJzkktiNd28qIAksKkhvll+qAppwOtFlrbNzGK7XJM8RzGO+vGRrlxQdAjDJmYWut/wcTakCg=='
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    url = 'https://ussouthcentral.services.azureml.net/workspaces/88cee1267b3f4781be5764f390ed1fa2/services/13bef76ee21148a6ba961ad4bdd6f0fa/execute?api-version=2.0&format=swagger'
+    api_key = 'v+QeyVdnxNfouQJzkktiNd28qIAksKkhvll+qAppwOtFlrbNzGK7XJM8RzGO+vGRrlxQdAjDJmYWut/wcTakCg=='
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-req = urllib.request.Request(url, body, headers)
+    req = urllib.request.Request(url, body, headers)
 
-try:
-    response = urllib.request.urlopen(req)
+    try:
+        response = urllib.request.urlopen(req)
 
-    result = response.read()
-    print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
+        result = response.read()
+        print(result)
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(json.loads(error.read().decode("utf8", 'ignore')))
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(json.loads(error.read().decode("utf8", 'ignore')))
